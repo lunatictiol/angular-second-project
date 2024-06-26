b22fdd69fc80e0fe148ade1f1afacdf1856418e0
“sabiq” <sabiq.a@myclnq.co.in>
2024-06-26 19:26:46 +0530

    add multi element custom components

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { InputComponent } from "../../../shared/control/input.component";
 
 @Component({
-  selector: 'app-new-ticket',
-  standalone: true,
-  imports: [],
-  templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+    selector: 'app-new-ticket',
+    standalone: true,
+    templateUrl: './new-ticket.component.html',
+    styleUrl: './new-ticket.component.css',
+    imports: [InputComponent]
 })
 export class NewTicketComponent {
 
